5786fe3842bdf8beb8eb47674f84f6853df2d668
sonapingo <sonapingo@163.com>
2022-04-12 22:45:38 +0800

    前端页面的准备工作

---
+++ client/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from '../views/index.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/index',
+  },
+  {
+    path: '/index',
+    name: 'index',
+    component: Index
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
